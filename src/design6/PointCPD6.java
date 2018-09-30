11defeff84107824e37c6ca30a76a8c18476c3e6
Suryadev Andotra <suryadevandotra13@gmail.com>
2018-09-29 21:06:52 -0400

    Finished Design 3 implementation with design 6

--- src/design6/PointCPD6.java
+++ src/design6/PointCPD6.java
@@ -1,18 +1,17 @@
 package design6;
 
-public interface PointCPD6<E> {
+public interface PointCPD6 {
 
 		public double getX();
 		public double getY();
 		public double getRho();
 		public double getTheta();
 		
+		/*public PointCPD6 convertStorageToPolar();*/
+		public PointCPD6 convertStorageToCartesian();
 		
-		public void convertStorageToPolar();
-		public void convertStorageToCartesian();
-		
-		public double getDistance(E pointB);
-		public E rotatePoint(double rotation);
+		public double getDistance(PointCPD6 pointB);
+		public PointCPD6 rotatePoint(double rotation);
 		
 		public String toString();
 		
